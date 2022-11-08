6c67f08753f7d7f31d66c0857134bdba8a80a56c
Kemps Medeiros <kempsmm@gmail.com>
2022-11-08 17:08:25 -0300

    feat: creating headers

---
+++ headers.js
@@ -0,0 +1,34 @@
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const APP_KEY_LATE = process.env.APP_KEY_LATE;
+
+
+export async function readToken() {
+    try {
+        const data = fs.readFileSync('./token.json');
+        const dataJson = JSON.parse(data);
+        return dataJson.token;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function  getHeaders() {
+    try {
+        const token = await readToken();
+        return {
+            Accept: "application/json",
+            "X-Application": APP_KEY_LATE,
+            "X-Authentication": token
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const headers = await getHeaders();
+// console.log(headers);
